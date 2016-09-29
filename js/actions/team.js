5d3ad436df1f4dffc34e33dfd760aca86e58db43
Kirill Stepkin <kirillstyopkin@gmail.com>
2016-09-29 14:20:45 +0300

    async await

--- js/actions/team.js
+++ js/actions/team.js
@@ -15,10 +15,18 @@ export function receiveTeam(team) {
 export function fetchTeam(team) {
 	return function (dispatch) {
 		dispatch(requestTeam(null))
-		return fetch(`/assets/football/Euro2016/${team}-players.json`)
-			.then(response => response.json())
-      .then(response =>
-				dispatch(receiveTeam(response.sheets.Players))
-			)
+
+		async function getTeamData() {
+			let data = await teamData(team);
+			dispatch(receiveTeam(data));
+		}
+
+		getTeamData();
 	}
+}
+
+async function teamData(team) {
+	let teamData = await fetch(`/assets/football/Euro2016/${team}-players.json`);
+	let res = await teamData.json();
+	return res.sheets.Players;
 }
