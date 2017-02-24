3ed942e48952b11851b93a9e78e7b6a9e3295477
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-02-24 16:07:42 +0200

    add some files from another repo

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    ejs = require('ejs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '.')));
+
+app.use('/', function(req, res) {
+    res.render('index');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('On');
+});
+
+module.exports = app;
