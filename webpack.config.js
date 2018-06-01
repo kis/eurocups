e12509aa720162e8df9a8286c119af04003ffaee
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-06-01 17:07:03 +0300

    webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,10 @@
-var webpack = require('webpack');
-var path = require('path');
+const TARGET = process.env.NODE_ENV;
 
-var autoprefixer = require('autoprefixer');
-var precss       = require('precss');
-var mixins       = require('postcss-mixins');
-
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-var appContext = path.join(__dirname, '/');
-
-var NODE_ENV = process.env.NODE_ENV || "production";
-console.log(NODE_ENV)
-
-module.exports = {
-  context: appContext,
-  mode: 'development',
-  entry: [
-    'babel-polyfill',
-    './js/index'
-  ],
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [{
-      test: /\.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      include: path.join(__dirname, '/js')
-    }, {
-      test: /\.css?$/,
-      use: [
-        'style-loader',
-        { loader: 'css-loader'},
-        { loader: 'postcss-loader', options: { postcss: [autoprefixer, precss, mixins], } }
-      ]
-    }, {
-      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-      loader: 'file-loader?name=[path][name].[ext]?[hash]'
-    }, {
-      test: /\.svg/,
-      loader: 'svg-url-loader'
-    }]
-  },
-  plugins: [
-    new ExtractTextPlugin("[name].css", { allChunks: true }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [autoprefixer, precss, mixins],
-      },
-    })
-  ],
-};
+if (TARGET === 'development') {
+    module.exports = require('./webpack.config.dev');
+    console.info('use ./webpack.config.dev.js');
+}
+if (TARGET === 'production') {
+    module.exports = require('./webpack.config.prod');
+    console.info('use ./webpack.config.prod.js');
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+var mixins       = require('postcss-mixins');
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var appContext = path.join(__dirname, '/');
+
+var NODE_ENV = process.env.NODE_ENV || "production";
+console.log('env', NODE_ENV)
+
+module.exports = {
+  context: appContext,
+  mode: 'production',
+  entry: [
+    'babel-polyfill',
+    './js/index'
+  ],
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devtool: 'eval-sourcemap',
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      include: path.join(__dirname, '/js')
+    }, {
+      test: /\.css?$/,
+      use: [
+        'style-loader',
+        { loader: 'css-loader'},
+        { loader: 'postcss-loader', options: { postcss: [autoprefixer, precss, mixins], } }
+      ]
+    }, {
+      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      loader: 'file-loader?name=[path][name].[ext]?[hash]'
+    }, {
+      test: /\.svg/,
+      loader: 'svg-url-loader'
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css", { allChunks: true })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all"
+        }
+      }
+    }
+  }
+};
