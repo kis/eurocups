212df354e16c437c347f465e603dcb32ebc498be
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-06-08 23:32:54 +0300

    add why-did-you-update

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import AppContainer from './containers/AppContainer';
 
+if (process.env.NODE_ENV !== 'production') {
+	const {whyDidYouUpdate} = require('why-did-you-update');
+	whyDidYouUpdate(React);
+}
+
 const App = () => (
 	<Provider store={store}>
 	    <AppContainer />
